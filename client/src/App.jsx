75249f465c637ea896cb5eae4a803a972dde1665
janneesa <janne.savinainen@hotmail.com>
2025-03-18 12:28:38 +0200

    implement user context and routing: add UserContext for user state management, integrate routing with react-router-dom, and update App component structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,17 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { useContext } from "react";
+import { UserContext } from "./context/UserContext";
+
 function App() {
+  const { user } = useContext(UserContext);
+
   return (
-    <>
-      <p className="text-3xl font-bold underline">Hello World</p>
-    </>
+    <div className="App">
+      <BrowserRouter>
+        <div className="App-content">moi</div>
+        <Routes></Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
